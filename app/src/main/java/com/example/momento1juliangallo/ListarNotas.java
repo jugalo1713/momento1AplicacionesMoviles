5caf936af0547952b191dbcb90c302b873c2b452
Julian Gallo <julianlondono@outlook.com>
2020-09-19 09:59:23 -0500

    este es el primer commit, incluye estructura básica de la actividad

---
+++ app/src/main/java/com/example/momento1juliangallo/ListarNotas.java
@@ -0,0 +1,14 @@
+package com.example.momento1juliangallo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListarNotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_notas);
+    }
+}
---
+++ app/src/main/java/com/example/momento1juliangallo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.momento1juliangallo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button registrar;
+    private Button listar;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registrar = findViewById(R.id.btn_registrar);
+        listar = findViewById(R.id.btn_listarNotas);
+
+
+    }
+}
