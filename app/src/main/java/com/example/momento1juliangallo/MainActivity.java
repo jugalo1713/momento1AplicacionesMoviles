7018d2868d24d53421db739fc16f069c9b65e61f
Julian Gallo <julianlondono@outlook.com>
2020-09-19 10:57:38 -0500

    Se crean los metodos on click para el main

--- app/src/main/java/com/example/momento1juliangallo/MainActivity.java
+++ app/src/main/java/com/example/momento1juliangallo/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.momento1juliangallo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button registrar;
     private Button listar;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,30 @@ public class MainActivity extends AppCompatActivity {
         registrar = findViewById(R.id.btn_registrar);
         listar = findViewById(R.id.btn_listarNotas);
 
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               registrar();
+            }
+        });
 
+        listar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listar();
+            }
+        });
     }
+
+    public void registrar ()
+    {
+        Intent intent = new Intent(this, RegistrarNotas.class);
+        startActivity(intent);
+    }
+    public void listar ()
+    {
+        Intent intent = new Intent(this, ListarNotas.class);
+        startActivity(intent);
+    }
+
 }
